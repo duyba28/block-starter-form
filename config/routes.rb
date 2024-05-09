87dd79cef475281f13dfe39ede34149d7b8f6633
duyba28 <duyba@remitano.com>
2024-05-09 16:06:48 +0700

    fix bug deploy

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      namespace :block_starter do
-        resources :projects, only: [:create, :show, :update, :destroy]
-      end
-    end
-  end
-end
